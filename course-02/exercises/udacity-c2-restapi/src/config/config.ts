f3a635971bbfe7ad32cc16251b64930e55050407
Elda Moudy <elda@moudy.net>
2020-05-24 17:18:33 -0700

    added AWS SDK get method

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -5,18 +5,21 @@ export const config = {
     "database": "udagramemoudydev",
     "host": "udagramemoudydev.csmihfmhnwyn.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_media_bucket": "udagram-emoudy-dev"
-  },
-  "aws": {
-    "aws_region": "",
-    "aws_profile": "",
-    "aws_media_bucket": ""
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "aws_media_bucket": "udagram-emoudy-dev",
+    "aws_region": "us-east-2",
+    "aws_profile": "default"
   }
+  // ,
+  // "aws": {
+  //   "aws_region": "us-east-2",
+  //   "aws_profile": "default",
+  //   "aws_media_bucket": "udagram-media-emoudy-dev"
+  // },
+  // "prod": {
+  //   "username": "",
+  //   "password": "",
+  //   "database": "udagram_prod",
+  //   "host": "",
+  //   "dialect": "postgres"
+  // }
 }
